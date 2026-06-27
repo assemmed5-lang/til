fed2b394c11d013f5153363f0d819ff2779ae2fe
assemmed5-lang <assemmed5@gmail.com>
2026-06-27 14:21:50 +0200

    Add conditional script injection with logging

--- inject.py
+++ inject.py
@@ -1,4 +1,10 @@
 content = open('data/banque_sourates.js', encoding='utf-8-sig').read()
 html = open('index.html', encoding='utf-8').read()
-html = html.replace('<script src="data/banque_sourates.js"></script>', '<script>' + content + '</script>')
+
+if '<script src="data/banque_sourates.js"></script>' in html:
+    print("TAG TROUVÉ - injection en cours")
+    html = html.replace('<script src="data/banque_sourates.js"></script>', '<script>' + content + '</script>')
+else:
+    print("TAG NON TROUVÉ - vérifier index.html")
+
 open('www/index.html', 'w', encoding='utf-8').write(html)
